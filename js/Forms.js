9871f38ada18d6fab4662d091fc26702923c21de
prashant <pkapde308@gmail.com>
2021-04-24 00:55:22 +0530

    Added:code for salary range button js

---
+++ js/Forms.js
@@ -0,0 +1,7 @@
+//Salary range button synchronised with values of user
+const salary = document.querySelector("#salary");
+const output = document.querySelector(".salary-output");
+output.textContent = salary.value;
+salary.addEventListener("input", function () {
+  output.textContent = salary.value;
+});
