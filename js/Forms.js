c893eb981148e03bd297408962f47b69b50207ed
prashant <pkapde308@gmail.com>
2021-04-24 00:56:52 +0530

    Added:code for name starts with capital and minimum 3 letters

--- js/Forms.js
+++ js/Forms.js
@@ -5,3 +5,13 @@ output.textContent = salary.value;
 salary.addEventListener("input", function () {
   output.textContent = salary.value;
 });
+
+//Name starts with capital letter and minimum 3 letters
+
+const text = document.querySelector("#text");
+const textError = document.querySelector(".text-error");
+text.addEventListener("input", function () {
+  let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
+  if (nameRegex.test(text.value)) textError.textContent = "";
+  else textError.textContent = "Name is Incorrect";
+});
